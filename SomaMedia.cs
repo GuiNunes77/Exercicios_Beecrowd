b03108b61c07fe907fffba520d19b3b222ac7f5a
GuiNunes77 <127356829+GuiNunes77@users.noreply.github.com>
2023-04-01 12:40:58 -0300

    Add files via upload

---
+++ SomaMedia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] valores = new double[6];
+        int qtdPositivos = 0;
+        double somaPositivos = 0;
+
+        for (int i = 0; i < 6; i++)
+        {
+            valores[i] = double.Parse(Console.ReadLine());
+            if (valores[i] > 0)
+            {
+                qtdPositivos++;
+                somaPositivos += valores[i];
+            }
+        }
+
+        Console.WriteLine(qtdPositivos + " valores positivos");
+        Console.WriteLine((somaPositivos / qtdPositivos).ToString("F1"));
+    }
+}
+
+
+
+
