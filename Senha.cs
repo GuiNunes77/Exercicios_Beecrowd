eed4e396129bb1a3b46eb9c332339a297b8f23e7
GuiNunes77 <127356829+GuiNunes77@users.noreply.github.com>
2023-04-01 12:18:27 -0300

    Add files via upload

---
+++ Senha.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Senha
+{
+    static void Main(string[] args)
+    {
+        int senha = 0;
+
+        do
+        {
+            Console.Write("Digite a senha: ");
+            senha = int.Parse(Console.ReadLine());
+
+            if (senha != 2002)
+            {
+                Console.WriteLine("Senha Invalida");
+            }
+        } while (senha != 2002);
+
+        Console.WriteLine("Acesso Permitido");
+    }
+}
